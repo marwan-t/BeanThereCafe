8f50f4320c8b63d39bcec35da289f31b1d51f250
Marwan Alghamdi <memo_1324@yahoo.com>
2022-06-24 12:43:37 +0300

    mispelling

--- been_there_cafe/lib/main.dart
+++ been_there_cafe/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return MaterialApp(
-      title: ' Been There Cafe',
+      title: ' Bean There Cafe',
       theme: ThemeData(
         // textTheme: GoogleFonts.pacificoTextTheme()
         // ),
